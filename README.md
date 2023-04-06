d9c71706d2a043d1f0ffa1f17521d1f8837db12b
FINDOSKDI <9279289+FINDOSKDI@users.noreply.github.com>
2023-04-06 22:21:48 +0000

    .

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Hello
+
+$ d save image:local > image.tar
+$ m ctr images ls
+$ m ctr image import image.tar
--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 gh_base="https://raw.githubusercontent.com/FINDOSKDI/bookish-couscous/main/"
 gh_=$gh_base
 gh=$gh_
+alias k="sudo kubectl"
+alias d="sudo docker"
+alias m="sudo microk8s"
 mv init.sh init.sh.old
